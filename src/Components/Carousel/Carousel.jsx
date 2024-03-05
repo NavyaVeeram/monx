b606495033a9fe98bb281bd2f44929cdbd9958b1
NavyaVeeram <navyareddyveeram2000@gmail.com>
2024-03-05 18:43:20 +0530

    message

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -2,13 +2,13 @@ import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function DarkVariantExample() {
   return (
-    <Carousel data-bs-theme="dark">
+    <Carousel data-bs-theme="dark" className='pt-5'>
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://img.freepik.com/free-photo/multi-colored-paint-drops-splashing-abstract-background-generated-by-ai_188544-15630.jpg?t=st=1709272200~exp=1709275800~hmac=d110e3dfbd365a3a14d295d4d870dd4dd9a2ef2d88d3174ec4741bf4e7164fe4&w=1380"
+          src="https://static.wixstatic.com/media/2fddf9_c27e762331ef4ca6ba351aad27a95897~mv2.jpg/v1/fill/w_1485,h_959,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/2fddf9_c27e762331ef4ca6ba351aad27a95897~mv2.jpg"
           alt="First slide"
-          style={{height:'500px'}}
+          style={{height:'auto'}}
         />
         <Carousel.Caption className='text-white'>
           <h5>First slide label</h5>
@@ -18,9 +18,9 @@ function DarkVariantExample() {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://img.freepik.com/free-photo/abstract-colorful-splash-3d-background-generative-ai-background_60438-2509.jpg?t=st=1709272278~exp=1709275878~hmac=f17d390d012f60d1c6daff144027fd0cdced431777b087f339af1df5d593e950&w=1380"
+          src="https://static.wixstatic.com/media/2fddf9_9afd87868081439f805784cc523e3c8b~mv2.jpg/v1/fill/w_1897,h_959,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/2fddf9_9afd87868081439f805784cc523e3c8b~mv2.jpg"
           alt="Second slide"
-          style={{height:'500px'}}
+          style={{height:'auto'}}
         />
         <Carousel.Caption className='text-white'>
           <h5>Second slide label</h5>
@@ -30,9 +30,9 @@ function DarkVariantExample() {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="https://img.freepik.com/free-photo/abstract-multi-colored-illustration-nature-vibrant-beauty-generated-by-ai_188544-15591.jpg?t=st=1709272311~exp=1709275911~hmac=dfb60769a36db26d76e670cc722bf58308de1e68a07f6de93a4346285d215007&w=1380"
+          src="https://static.wixstatic.com/media/2fddf9_9afd87868081439f805784cc523e3c8b~mv2.jpg/v1/fill/w_1897,h_959,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/2fddf9_9afd87868081439f805784cc523e3c8b~mv2.jpg"
           alt="Third slide"
-          style={{height:'500px'}}
+          style={{height:'auto'}}
         />
         <Carousel.Caption className='text-white'>
           <h5>Third slide label</h5>
